5b3e9e0c08ed3b64ce934fd166f997f66863df99
Kateryna Strough <katerynastrough@hotmail.com>
2019-05-04 22:25:15 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -13,7 +13,10 @@ def first_challenge
     }
   }
 
-  contacts.delete     #your code here
+  contacts["Freddy Mercury"].each do |attribute, value|
+      if attribute == :favorite_icecream_flavors
+        value.delete_if do |flavor|
+          flavor == "strawberry"    #your code here
 
 
   #remember to return your newly altered contacts hash!
